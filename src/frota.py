41005915921b3f2315838f5e11fda81488d1f230
rhuanmarcos <rhuanoliveira2004@gmail.com>
2024-08-26 21:38:35 -0300

    Aula 3

--- src/frota.py
+++ src/frota.py
@@ -2,42 +2,66 @@ class Carro:
     modelo : str
     marca : str
     cor : str
-    odometro = 0.0
-    motor_on = False
+    __odometro : 0.0
+    __motor_on : False
+    __tanque: float
+    consumo_medio: float
 
     def __init__(self, modelo : str, marca : str, cor : str,
-                       odometro : float, motor : bool):
+                       odometro : float, motor : bool, tanque: float, consumo_medio: float):
         self.modelo = modelo
         self.marca = marca
         self.cor = cor
-        self.odometro = odometro
-        self.motor_on = motor
+        self.__odometro = odometro
+        self.__motor_on = motor
+        self.__tanque = tanque
+        self.consumo_medio = consumo_medio
 
     def ligar(self):
-        if not self.motor_on:
-            self.motor_on = True
+        if not self.__motor_on:
+            self.__motor_on = True
         else:
             raise Exception("Erro: Motor já ligado!")
 
     def acelerar(self, velocidade : float, tempo : float):
-        if self.motor_on:
-            self.odometro += velocidade * tempo
+        if self.__motor_on and self.__tanque > 0:
+            km = velocidade * tempo
+            litros = km / self.consumo_medio
+
+            if self.__tanque >= litros:
+                self.__odometro += km
+                self.__tanque -= litros
+            else:
+                km = self.__tanque * self.consumo_medio
+                self.__odometro += km
+                self.__tanque = 0
         else:
-            raise Exception("Erro: Não é possível acelerar! Motor desligado!")
+            raise Exception("Erro: Não é possível acelerar! Motor desligado ou sem combustível!")
+
 
     def desligar(self):
-        if self.motor_on:
-            self.motor_on = False
+        if self.__motor_on:
+            self.__motor_on = False
         else:
             raise Exception("Erro: Motor já desligado!")
 
+    def getOdometro(self):
+        return self.__odometro
+
+    def getTanque(self):
+        return self.__tanque
+
+    def getMotorOn(self):
+        return self.__motor_on
+
     def __str__(self):
         info = (f'Carro {self.modelo}, marca {self.marca}, '
-                f'cor {self.cor}\n{self.odometro} Km, '
-                f'motor {self.motor_on}')
+                f'cor {self.cor}\n{self.__odometro} Km, '
+                f'motor {self.__motor_on}, consumo medio {self.consumo_medio}\n'
+                f'tanque {self.__tanque}')
         return info
 
-
-
-
-
+    def __repr__(self):
+        return f'Carro(modelo="{self.modelo}", marca="{self.marca}", ' \
+               f'cor="{self.cor}", odometro={self.__odometro}' \
+               f'motor_on={self.__motor_on})'
--- src/main.py
+++ src/main.py
@@ -1,39 +1,75 @@
 from frota import *
+import pickle
+
+def operar(opcao : Carro):
+    print(f'1- Ligar motor do {opcao.modelo}')
+    print(f'2- Desligar motor do {opcao.modelo}')
+    print(f'3- Acelerar do {opcao.modelo}')
+
+    op = 0
+    while op not in (1, 2, 3):
+        op = int(input("Digite as opcoes[1-3]: "))
+
+    if op == 1:
+        opcao.ligar()
+    elif op == 2:
+        opcao.desligar()
+    elif op == 3:
+        v = float(input(f"Informe a velocidade do {opcao.modelo}: "))
+        t = float(input(f"Informe o tempo do {opcao.modelo}: "))
+        opcao.acelerar(v, t)
+
+    print('Infos atuais do carro')
+    print(opcao)
 
 if __name__ == "__main__":
-    print('Cadastre um carro')
+    print('Cadastre o carro 1')
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
+    nm_consumoMedio = float(input('Digite o consumo medio: '))
+    nm_tanque = float(input('Digite o nivel do tanque: '))
 
-    kms = float(input('Digite com quantos Kms: '))
+    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, False, nm_tanque, nm_consumoMedio)
 
-    carro1 = Carro(nm_modelo, nm_marca, nm_cor, kms, motor = True)
+    print('Cadastre o carro 2')
+    nm_modelo = input('Digite o modelo: ')
+    nm_marca = input('Digite a marca: ')
+    nm_cor = input('Digite a cor: ')
+    nm_consumoMedio = float(input('Digite o consumo medio: '))
+    nm_tanque = float(input('Digite o nivel do tanque: '))
+
+    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, False, nm_tanque, nm_consumoMedio)
+
+    carros = {}
+    carros[id(carro1)] = carro1
+    carros[id(carro2)] = carro2
+
+    try:
+        with open('carros.pkl', 'wb') as arquivo:
+            pickle.dump(carros, arquivo)
+    except Exception as e:
+        print(e)
 
     '''
     Controlando o carro até ele atingir 10000 Km
     '''
-    while carro1.odometro < 10000:
+
+    while carro1.getOdometro() < 300 and carro2.getOdometro() < 300 and (carro1.getTanque() > 0 or carro2.getTanque() > 0):
         try:
-            print('1- Ligar motor')
-            print('2- Desligar motor')
-            print('3- Acelerar')
+            print('Qual carro deseja utilizar ?')
 
-            op = 0
-            while op not in (1,2,3):
-                op = int(input("Digite as opcoes[1-3]: "))
+            opCarro = 0
+            while opCarro not in (1, 2):
+                opCarro = int(input('1 ou 2: '))
 
-            if op == 1:
-                carro1.ligar()
-            elif op == 2:
-                carro1.desligar()
-            elif op == 3:
-                v = float(input("Informe a velocidade: "))
-                t = float(input("Informe o tempo: "))
-                carro1.acelerar(v, t)
+                if opCarro == 1:
+                    operar(carro1)
+                elif opCarro == 2:
+                    operar(carro2)
+                else:
+                    raise Exception("Opcao invalida!!")
 
-            print('Infos atuais do carro')
-            print(carro1)
         except Exception as e:
             print("Erro!")
             print(e)
@@ -41,4 +77,3 @@ if __name__ == "__main__":
     carro1.desligar()
     print(carro1)
     print('Parar para trocar óleo!!!')
-
